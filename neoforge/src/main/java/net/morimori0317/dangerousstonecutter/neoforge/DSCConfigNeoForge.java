3b4605d62e06f81487c9c3d2dd454f77cfd2f84f
MORIMORI0317 <morimori.0317@outlook.jp>
2026-08-24 01:37:33 +0900

    Fix Error

--- neoforge/src/main/java/net/morimori0317/dangerousstonecutter/neoforge/DSCConfigNeoForge.java
+++ neoforge/src/main/java/net/morimori0317/dangerousstonecutter/neoforge/DSCConfigNeoForge.java
@@ -4,18 +4,24 @@ import net.morimori0317.dangerousstonecutter.DSCConfig;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.ModConfigSpec;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class DSCConfigNeoForge implements DSCConfig {
+    public static final Logger LOGGER = LogManager.getLogger(DSCConfigNeoForge.class);
+
     private static ModConfigSpec.ConfigValue<Double> DAMAGE;
     private static ModConfigSpec.ConfigValue<Boolean> ENABLE_BLOOD_PARTICLE;
     private static ModConfigSpec.ConfigValue<Boolean> ENABLE_MOB_AVOID_STONECUTTER;
+
+    public static ModConfigSpec COMMON_CONFIG;
 
     public static void init(ModContainer container) {
         var serverConfig = buildServerConfig(new ModConfigSpec.Builder()).build();
         container.registerConfig(ModConfig.Type.SERVER, serverConfig);
 
-        var commonConfig = buildCommonConfig(new ModConfigSpec.Builder()).build();
-        container.registerConfig(ModConfig.Type.COMMON, commonConfig);
+        COMMON_CONFIG = buildCommonConfig(new ModConfigSpec.Builder()).build();
+        container.registerConfig(ModConfig.Type.COMMON, COMMON_CONFIG);
     }
 
     private static ModConfigSpec.Builder buildServerConfig(ModConfigSpec.Builder builder) {
@@ -36,6 +42,12 @@ public class DSCConfigNeoForge implements DSCConfig {
 
     @Override
     public boolean isEnableMobAvoidStonecutter() {
+
+        if (!COMMON_CONFIG.isLoaded()) {
+            LOGGER.warn("Config is not loaded!");
+            return false;
+        }
+
         return ENABLE_MOB_AVOID_STONECUTTER.get();
     }
 
